e49dd0b944548a442377c2ddd6c45792c1bc9420
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-01-19 12:54:56 -0300

    feat: criando a entidade Product

---
+++ desafio-anota-ai/src/main/java/com/gabriel/desafioanotaai/domain/model/product/Product.java
@@ -0,0 +1,23 @@
+package com.gabriel.desafioanotaai.domain.model.product;
+
+import com.gabriel.desafioanotaai.domain.model.category.Category;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.aggregation.StringOperators;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document("products")
+public class Product {
+    @Id
+    private String id;
+    private String title;
+    private String description;
+    private String ownerId;
+    private Integer price;
+    private Category category;
+}
