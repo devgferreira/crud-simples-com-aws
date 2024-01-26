4e79bdf6cec2676f7db27b901f31343960d34c74
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-01-26 10:44:21 -0300

    feat: criando o contrato getAll no ICategoryService

--- desafio-anota-ai/src/main/java/com/gabriel/desafioanotaai/application/interfaces/ICategoryService.java
+++ desafio-anota-ai/src/main/java/com/gabriel/desafioanotaai/application/interfaces/ICategoryService.java
@@ -2,6 +2,10 @@ package com.gabriel.desafioanotaai.application.interfaces;
 
 import com.gabriel.desafioanotaai.application.dtos.CategoryDTO;
 
+import java.util.List;
+
 public interface ICategoryService {
      CategoryDTO createCategory(CategoryDTO categoryDTO);
+     List<CategoryDTO> getAll();
+
 }
