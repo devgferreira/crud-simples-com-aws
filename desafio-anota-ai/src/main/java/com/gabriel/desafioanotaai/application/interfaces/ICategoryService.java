f91c519554b0570febe783669e882363314e4906
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-01-26 10:00:30 -0300

    feat: criando a interface: ICategoryService e um método: createCategory

---
+++ desafio-anota-ai/src/main/java/com/gabriel/desafioanotaai/application/interfaces/ICategoryService.java
@@ -0,0 +1,7 @@
+package com.gabriel.desafioanotaai.application.interfaces;
+
+import com.gabriel.desafioanotaai.application.dtos.CategoryDTO;
+
+public interface ICategoryService {
+     CategoryDTO createCategory(CategoryDTO categoryDTO);
+}
