7a858e0ad83aebc15354f93757033b3096211aab
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-01-26 10:04:22 -0300

    fix: adicionando a anotação @Configuration no MongoDBConfig

--- desafio-anota-ai/src/main/java/com/gabriel/desafioanotaai/infra/config/mongo/MongoDBConfig.java
+++ desafio-anota-ai/src/main/java/com/gabriel/desafioanotaai/infra/config/mongo/MongoDBConfig.java
@@ -1,10 +1,12 @@
 package com.gabriel.desafioanotaai.infra.config.mongo;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 
+@Configuration
 public class MongoDBConfig {
 
     public MongoDatabaseFactory mongoConfigure(){
