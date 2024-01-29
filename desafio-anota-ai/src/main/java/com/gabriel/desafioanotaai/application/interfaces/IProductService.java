03b0857330a279624315129ecb0571b7f5e9c6ef
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-01-29 20:34:19 -0300

    feat: criando o método de crianção de um Product

--- desafio-anota-ai/src/main/java/com/gabriel/desafioanotaai/application/interfaces/IProductService.java
+++ desafio-anota-ai/src/main/java/com/gabriel/desafioanotaai/application/interfaces/IProductService.java
@@ -1,12 +1,15 @@
 package com.gabriel.desafioanotaai.application.interfaces;
 
 
+import com.gabriel.desafioanotaai.application.dtos.CategoryDTO;
 import com.gabriel.desafioanotaai.application.dtos.ProductDTO;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IProductService {
-     ProductDTO createProduct(ProductDTO categoryDTO);
+
+     ProductDTO createProduct(ProductDTO productDTO);
      List<ProductDTO> getAll();
      ProductDTO updateProduct(String id, ProductDTO productDTO);
      void deleteProduct(String id);
---
+++ desafio-anota-ai/src/main/java/com/gabriel/desafioanotaai/application/services/ProductService.java
@@ -0,0 +1,50 @@
+package com.gabriel.desafioanotaai.application.services;
+
+import com.gabriel.desafioanotaai.application.dtos.CategoryDTO;
+import com.gabriel.desafioanotaai.application.dtos.ProductDTO;
+import com.gabriel.desafioanotaai.application.interfaces.ICategoryService;
+import com.gabriel.desafioanotaai.application.interfaces.IProductService;
+import com.gabriel.desafioanotaai.domain.model.category.Category;
+import com.gabriel.desafioanotaai.domain.model.product.Product;
+import com.gabriel.desafioanotaai.domain.repository.IProductRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService implements IProductService {
+    private final ModelMapper _modelMapper;
+    private final IProductRepository _productRepository;
+
+    private final ICategoryService _categoryService;
+
+    public ProductService(ModelMapper modelMapper, IProductRepository productRepository, ICategoryService categoryService) {
+        _modelMapper = modelMapper;
+        _productRepository = productRepository;
+        _categoryService = categoryService;
+    }
+    @Override
+    public ProductDTO createProduct(ProductDTO productDTO) {
+        CategoryDTO categoryDTO = _categoryService.getById(productDTO.getCategory().getId()).orElseThrow();
+        Product product = _modelMapper.map(productDTO, Product.class);
+        product.setCategory(_modelMapper.map(categoryDTO, Category.class));
+        return _modelMapper.map(_productRepository.save(product), ProductDTO.class);
+    }
+
+    @Override
+    public List<ProductDTO> getAll() {
+        return null;
+    }
+
+    @Override
+    public ProductDTO updateProduct(String id, ProductDTO productDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(String id) {
+
+    }
+}
