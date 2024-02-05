35a7095d86e86dfd84bfd5094b524dfc49990d30
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-02-05 16:12:29 -0300

    fix: mudando o retorno do getAll para List<Product> no IProductService

--- desafio-anota-ai/src/main/java/com/gabriel/desafioanotaai/application/interfaces/IProductService.java
+++ desafio-anota-ai/src/main/java/com/gabriel/desafioanotaai/application/interfaces/IProductService.java
@@ -3,6 +3,7 @@ package com.gabriel.desafioanotaai.application.interfaces;
 
 import com.gabriel.desafioanotaai.application.dtos.CategoryDTO;
 import com.gabriel.desafioanotaai.application.dtos.ProductDTO;
+import com.gabriel.desafioanotaai.domain.model.product.Product;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,7 +11,7 @@ import java.util.Optional;
 public interface IProductService {
 
      ProductDTO createProduct(ProductDTO productDTO);
-     List<ProductDTO> getAll();
+     List<Product> getAll();
      ProductDTO updateProduct(String id, ProductDTO productDTO);
      void deleteProduct(String id);
 
