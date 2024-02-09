d55b52cae200dfdb08b5143511bc08b82453ab31
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-02-09 15:21:22 -0300

    test: Criando a classe de teste ProductServiceTest e o teste: createProductTest_ComCategoryValida_RetornandoProduct

---
+++ desafio-anota-ai/src/test/java/com/gabriel/desafioanotaai/application/services/ProductServiceTest.java
@@ -0,0 +1,65 @@
+package com.gabriel.desafioanotaai.application.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.gabriel.desafioanotaai.application.dtos.ProductDTO;
+import com.gabriel.desafioanotaai.domain.model.category.Category;
+import com.gabriel.desafioanotaai.domain.model.product.Product;
+import com.gabriel.desafioanotaai.domain.repository.ICategoryRepository;
+import com.gabriel.desafioanotaai.domain.repository.IProductRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+
+    @InjectMocks
+    private ProductService _productService;
+
+    @Mock
+    private IProductRepository _productRepository;
+
+    @Mock
+    private CategoryService _categoryService;
+
+    @Mock
+    private ModelMapper _modelMapper;
+
+    @Test
+    void createProductTest_ComCategoryValida_RetornandoProduct(){
+        Category category = new Category("123123123ASDASDAS", "Teste", "Teste Descrição", "123");
+        ProductDTO productDTO = new ProductDTO("Teste123", "Teste", "Descrição", "123", 12345, category.getId());
+
+        Product product = new Product(productDTO);
+        product.setCategory(category);
+
+
+        when(_modelMapper.map(productDTO, Product.class)).thenReturn(product);
+        when(_productRepository.save(product)).thenReturn(product);
+        when(_categoryService.getById(category.getId())).thenReturn(Optional.of(category));
+        when(_modelMapper.map(product, ProductDTO.class)).thenReturn(productDTO);
+
+
+        ProductDTO result = _productService.createProduct(productDTO);
+
+        assertEquals(result, productDTO);
+    }
+
+}
