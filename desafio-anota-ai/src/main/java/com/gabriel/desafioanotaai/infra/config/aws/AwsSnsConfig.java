5345945452edb2422451064d606f22f00d8ce868
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-02-12 12:30:34 -0300

    feat: configurando o acesso ao Topic do SNS com o método: snsCatalogTopicBuilder - AwsSnsConfig

--- desafio-anota-ai/src/main/java/com/gabriel/desafioanotaai/infra/config/aws/AwsSnsConfig.java
+++ desafio-anota-ai/src/main/java/com/gabriel/desafioanotaai/infra/config/aws/AwsSnsConfig.java
@@ -4,6 +4,7 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sns.model.Topic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,6 +30,11 @@ public class AwsSnsConfig {
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(region)
                 .build();
+    }
+
+    @Bean(name = "catalogEventTopic")
+    public Topic snsCatalogTopicBuilder(){
+        return new Topic().withTopicArn(catalogTopicArn);
     }
 
 }
